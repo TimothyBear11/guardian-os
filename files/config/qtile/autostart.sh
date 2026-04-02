757633df1ba000ed9b2bb564ce5be172e9f5da84
TimothyBear11 <timothybear1183@pm.me>
2026-04-02 16:13:11 -0400

    updating for niri and qtile

--- files/config/qtile/autostart.sh
+++ files/config/qtile/autostart.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# 1. Start the D-Bus session and export it to the environment
-dbus-update-activation-environment --systemd --all &
-
-# 2. Initialize the Gnome Keyring (The "Login Memory" Fix)
-eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh) &
-export SSH_AUTH_SOCK
 
 # 3. Start the wallpaper daemon early
 swww-daemon &
